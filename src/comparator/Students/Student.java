fe328bc441c97a206284d7e08c6fbb7b67749718
Lkarion <97668616+Lkarion@users.noreply.github.com>
2022-05-14 21:42:19 +0200

    new Student

--- src/comparator/Students/Student.java
+++ src/comparator/Students/Student.java
@@ -1,6 +1,10 @@
 package comparator.Students;
 
-public class Student {
+import comparator.Observe.Observer;
+import comparator.Observe.Task;
+import comparator.Observe.TaskState;
+
+public class Student implements Observer {
     //Create new class Students with following fields (String name, int yearOfBirthday, double gpa).
     // Create the array of several students. Your program should ask user how to sort students
     // (by name, by age, or by gpa) and display students accordingly
@@ -14,9 +18,20 @@ public class Student {
         this.gpa = gpa;
     }
 
+    public String getName() {
+        return name;
+    }
+
     @Override
     public String toString() {
         return name + ' ' + yearOfBirthday + ", " + gpa;
     }
 
+
+    @Override
+    public void handleTask(Task task) {
+        System.out.println("Student " + name + " got Task id" + task.getTaskID());
+        task.setState(TaskState.IN_PROCESS);
+        task.setState(TaskState.DONE);
+    }
 }
